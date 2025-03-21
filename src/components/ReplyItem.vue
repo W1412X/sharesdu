5da9a7bb3f365e5384752c3c7bab8f414529087b
W1412X <tor108@outlook.com>
2025-03-21 20:16:38 +0800

    2025.3.21

--- src/components/ReplyItem.vue
+++ src/components/ReplyItem.vue
@@ -1,59 +1,60 @@
 <template>
-    <v-dialog v-model="ifShowDialog" style="display: flex;flex-direction: row;align-items: center;justify-content: center;width: 100%;height: 100%;">
-        <v-card v-if="ifShowReplyEditor" class="dialog-card">
-            <div class="title-bold">
-                回复评论
-            </div>
-            <SensitiveTextArea v-model="replyContent"  style="margin-top: 10px;" variant="outlined" density="compact" label="输入评论内容"/>
-            <div class="dialog-bottom-btn-bar">
-                <v-btn @click="reply" variant="text">发表</v-btn>
-                <v-btn @click="setReplyEditorState(false)" variant="text">取消</v-btn>
-            </div>
-        </v-card>
+    <v-dialog v-model="ifShowDialog"
+        style="display: flex;flex-direction: row;align-items: center;justify-content: center;width: 100%;height: 100%;">
+        <div style="width: 100%;height:100%;justify-content: center;display: flex">
+            <v-card v-if="ifShowReplyEditor" class="dialog-card">
+                <div class="title-bold">
+                    回复评论
+                </div>
+                <SensitiveTextArea v-model="replyContent" style="margin-top: 10px;" variant="outlined" density="compact"
+                    label="输入评论内容" />
+                <div class="dialog-bottom-btn-bar">
+                    <v-btn @click="reply" variant="text">发表</v-btn>
+                    <v-btn @click="setReplyEditorState(false)" variant="text">取消</v-btn>
+                </div>
+            </v-card>
+        </div>
+
     </v-dialog>
     <v-card elevation="1" v-if="!ifDeleted" class="container">
-        <div
-            class="name text-medium"
-        >
-            <avatar-name v-if="data.authorId" :initData="{id:data.authorId,name:data.authorName}"></avatar-name>
+        <div class="name text-medium">
+            <avatar-name v-if="data.authorId" :initData="{ id: data.authorId, name: data.authorName }"></avatar-name>
         </div>
-        <div
-             @click="click"
-            class="comment text-medium"
-        >   
-            <span v-if="this.ifChild==true" @click="showParent" class="text-medium-bold" :style="{'color':themeColor}">{{ parentAuthorName+'： ' }}</span>
+        <div @click="click" class="comment text-medium">
+            <span v-if="this.ifChild == true" @click="showParent" class="text-medium-bold"
+                :style="{ 'color': themeColor }">{{ parentAuthorName + '： ' }}</span>
             {{ data.content }}
         </div>
-        <div
-            class="bottom-bar"
-        >
+        <div class="bottom-bar">
             <div class="time text-small">
                 <v-icon size="18" icon="mdi-clock-outline" style="margin-right: 5px;"></v-icon>
                 <span>{{ this.data.publishTime }}</span>
             </div>
             <v-spacer></v-spacer>
             <div class="bottom-btn-container">
-                <like-button @alert="alert" @set_loading="setLoading" :size="'20'" :id="this.data.id" :state="this.data.ifLike" :type="'reply'"></like-button>
+                <like-button @alert="alert" @set_loading="setLoading" :size="'20'" :id="this.data.id"
+                    :state="this.data.ifLike" :type="'reply'"></like-button>
             </div>
             <div class="like-num text-small">
                 {{ this.data.likeNum }}
             </div>
             <div style="margin-right: 10px;">
-                    <v-btn @click="setReplyEditorState(true)" elevation="0" icon :style="{
-                        'width': '20px',
-                        'height': '20px',
-                        'color': '#8a8a8a',
-                        'background-color': 'rgba(0,0,0,0)',
-                        'margin-bottom':'5px'
-                    }">
-                        <v-icon :size="'23'" icon="mdi-reply-outline"></v-icon>
-                    </v-btn>
+                <v-btn @click="setReplyEditorState(true)" elevation="0" icon :style="{
+                    'width': '20px',
+                    'height': '20px',
+                    'color': '#8a8a8a',
+                    'background-color': 'rgba(0,0,0,0)',
+                    'margin-bottom': '5px'
+                }">
+                    <v-icon :size="'23'" icon="mdi-reply-outline"></v-icon>
+                </v-btn>
             </div>
-            <div v-if="userId!=data.authorId" style="margin-right: 10px;">
+            <div v-if="userId != data.authorId" style="margin-right: 10px;">
                 <alert-button :size="'20'" :id="this.data.id" :type="'reply'"></alert-button>
             </div>
             <div v-else style="margin-right: 10px;">
-                <delete-button @delete="deleteSelf" :id="this.data.id" :type="'reply'" :size="20" @alert="alert" @set_loading="setLoading"></delete-button>
+                <delete-button @delete="deleteSelf" :id="this.data.id" :type="'reply'" :size="20" @alert="alert"
+                    @set_loading="setLoading"></delete-button>
             </div>
         </div>
     </v-card>
@@ -81,7 +82,7 @@ export default {
                     publishTime: null,
                     likeNum: null,
                     content: null,
-                    authorId:null,
+                    authorId: null,
                 }
             }
         },
@@ -90,95 +91,96 @@ export default {
             default: null
         },
     },
-    setup(){
-        const themeColor=globalProperties.$themeColor;
-        const userId=getCookie("userId");
-        const ifShowReplyEditor=ref(false);
-        const ifShowDialog=computed(()=>{
+    setup() {
+        const themeColor = globalProperties.$themeColor;
+        const userId = getCookie("userId");
+        const ifShowReplyEditor = ref(false);
+        const ifShowDialog = computed(() => {
             return ifShowReplyEditor.value;
         })
-        const setReplyEditorState=(state)=>{
-            ifShowReplyEditor.value=state;
+        const setReplyEditorState = (state) => {
+            ifShowReplyEditor.value = state;
         }
-        return {userId,
+        return {
+            userId,
             themeColor,
             ifShowDialog,
             ifShowReplyEditor,
             setReplyEditorState,
         };
-     },
-    components:{
+    },
+    components: {
         AlertButton,
         LikeButton,
         AvatarName,
         DeleteButton,
         SensitiveTextArea,
     },
-    data(){
-        const data=this.initData;
-        return{
+    data() {
+        const data = this.initData;
+        return {
             data,
-            ifDeleted:false,
-            ifChild:false,
-            parentAuthorName:null,
-            parentReplyId:null,
-            childReplys:[],
-            replyContent:"",
+            ifDeleted: false,
+            ifChild: false,
+            parentAuthorName: null,
+            parentReplyId: null,
+            childReplys: [],
+            replyContent: "",
         }
     },
     methods: {
-        click(){
+        click() {
 
         },
-        alert(msg){
-            this.$emit('alert',msg);
+        alert(msg) {
+            this.$emit('alert', msg);
         },
-        setLoading(msg){
-            this.$emit('set_loading',msg);
+        setLoading(msg) {
+            this.$emit('set_loading', msg);
         },
-        deleteSelf(){
-            this.ifDeleted=true;
+        deleteSelf() {
+            this.ifDeleted = true;
         },
-        showParent(){
-            this.$emit('show_parent',this.parentReplyId);
+        showParent() {
+            this.$emit('show_parent', this.parentReplyId);
             console.log(this.parentReplyId);
         },
-        async reply(){
-            if(this.replyContent.length<=5){
+        async reply() {
+            if (this.replyContent.length <= 5) {
                 this.alert(getNormalWarnAlert("评论内容过短"));
                 return;
             }
-            let content=addHeaderToReply(this.replyContent,this.data.authorName,this.data.id);
+            let content = addHeaderToReply(this.replyContent, this.data.authorName, this.data.id);
             console.log(content);
             this.setLoading(getLoadMsg("正在提交评论..."));
-            let response=await createReplyUnderPost(this.postId,content,this.data.id);
-            this.setLoading(getCancelLoadMsg());     
-            if(response.status==200||response.status==201){
+            let response = await createReplyUnderPost(this.postId, content, this.data.id);
+            this.setLoading(getCancelLoadMsg());
+            if (response.status == 200 || response.status == 201) {
                 this.alert(getNormalSuccessAlert("评论成功"));
-                this.replyContent="";
-                this.$emit("reply",{
-                    id:response.reply_id,
-                    content:content,
-                    authorName:getCookie("userName"),
-                    authorId:getCookie("userId"),
-                    likeNum:0,
-                    publishTime:new Date().toLocaleString(),
+                this.replyContent = "";
+                this.$emit("reply", {
+                    id: response.reply_id,
+                    content: content,
+                    authorName: getCookie("userName"),
+                    authorId: getCookie("userId"),
+                    likeNum: 0,
+                    publishTime: new Date().toLocaleString(),
                 })
                 this.setReplyEditorState(false);
-            }else{
+            } else {
                 this.alert(getNormalErrorAlert(response.message));
             }
         }
     },
-    async created(){
+    async created() {
         //deal with the reply with parent  
-        if(this.data.content.startsWith("@")){
-            try{
-                this.parentAuthorName=getAuthorNameFromReply(this.data.content);
-                this.parentReplyId=getParentReplyIdFromReply(this.data.content);
-                this.data.content=getReplyContentWithoutHeader(this.data.content);
-                this.ifChild=true;
-            }catch(e){
+        if (this.data.content.startsWith("@")) {
+            try {
+                this.parentAuthorName = getAuthorNameFromReply(this.data.content);
+                this.parentReplyId = getParentReplyIdFromReply(this.data.content);
+                this.data.content = getReplyContentWithoutHeader(this.data.content);
+                this.ifChild = true;
+            } catch (e) {
                 return;
             }
         }
@@ -186,31 +188,37 @@ export default {
 }
 </script>
 <style scoped>
-.bottom-bar{
+.bottom-bar {
     display: flex;
     flex-direction: row;
     align-items: center;
     margin-top: 5px;
 }
-.bottom-line{
+
+.bottom-line {
     margin-top: 5px;
     border-bottom: #8a8a8a 1px solid;
 }
-.column-center{
+
+.column-center {
     display: flex;
     flex-direction: row;
     align-items: center;
 }
-.dialog-card{
+
+.dialog-card {
     padding: 10px;
     display: flex;
+    max-width: 400px;
     flex-direction: column;
 }
-.dialog-bottom-btn-bar{
+
+.dialog-bottom-btn-bar {
     display: flex;
     flex-direction: row-reverse;
 }
-.like-num{
+
+.like-num {
     display: flex;
     align-items: center;
     height: 100%;
@@ -220,12 +228,13 @@ export default {
     max-width: 100px;
     align-items: center;
     color: grey;
-    white-space:nowrap; 
-    overflow:hidden;
-    text-overflow:ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
+
 @media screen and (min-width: 600px) {
-    .container{
+    .container {
         width: 900px;
         display: flex;
         flex-direction: column;
@@ -233,12 +242,14 @@ export default {
         padding-left: 15px;
         padding-right: 15px;
     }
-    .name{
+
+    .name {
         width: 100%;
         display: flex;
         margin-bottom: 5px;
     }
-    .time{
+
+    .time {
         flex-direction: row;
         display: flex;
         color: grey;
@@ -249,7 +260,7 @@ export default {
 }
 
 @media screen and (max-width: 600px) {
-    .container{
+    .container {
         width: 100%;
         display: flex;
         flex-direction: column;
@@ -257,12 +268,14 @@ export default {
         padding-left: 10px;
         padding-right: 10px;
     }
-    .name{
+
+    .name {
         width: 100%;
         display: flex;
         margin-bottom: 5px;
     }
-    .time{
+
+    .time {
         flex-direction: row;
         display: flex;
         margin-top: 5px;
